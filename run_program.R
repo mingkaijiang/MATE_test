eec3582aa4876eea3732ae9598dace98afc8382a
mingkaijiang <shanyujordan512@gmail.com>
2020-05-22 12:04:47 +1000

    lunch

--- run_program.R
+++ run_program.R
@@ -23,13 +23,13 @@ rundfr <- expand.grid(Ca=c(380,620), Wcapac=c(60,80,100,120,140,160,180,200))
 rundfr$PAW0 <- rundfr$Wcapac
 res1 <- RMATEsensitivity(rundfr)
 
-# rundfr$NPP <- sapply(res1, function(x)mean(x$NPPtCha))
-# rundfr$m <- sapply(res1, function(x)mean(x$m))
-# rundfr$PAW <- sapply(res1, function(x)mean(x$PAWcur))
+rundfr$NPP <- sapply(res1, function(x)mean(x$NPPtCha))
+rundfr$m <- sapply(res1, function(x)mean(x$m))
+rundfr$PAW <- sapply(res1, function(x)mean(x$PAWcur))
 
-# # example
-# with(rundfr, plot(PAW0, PAW, pch=19, col=nicecolors[as.factor(Ca)]))
-# legend("bottomright",levels(as.factor(rundfr$Ca)),pch=19,col=nicecolors)
+# example
+with(rundfr, plot(PAW0, PAW, pch=19, col=as.factor(Ca)))
+legend("bottomright",levels(as.factor(rundfr$Ca)),pch=19,col=as.factor(Ca))
 
 ################################### end run MATE ######################################
 #######################################################################################
