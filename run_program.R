3cb638ee76af958958ea2e6676ae58d4bdd374ef
mingkaijiang <shanyujordan512@gmail.com>
2020-05-22 10:33:15 +1000

    set up

---
+++ run_program.R
@@ -0,0 +1,7 @@
+#### This is the master script to run the MATE model,
+#### based on codes developed by Belinda Medlyn. 
+#### Contact: Mingkai Jiang (m.jiang@westernsydney.edu.au). 
+
+
+
+run_MATE
