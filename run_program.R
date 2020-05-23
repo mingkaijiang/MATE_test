3942741c3ed51d327f37ee5247bd209faecbe53b
mingkaijiang <shanyujordan512@gmail.com>
2020-05-23 13:41:22 +1000

    removing sensitivity test

--- run_program.R
+++ run_program.R
@@ -18,15 +18,20 @@ source("parameter/parameters.R")
 
 #######################################################################################
 ################################### run MATE ##########################################
-#### run
-rundfr <- expand.grid(#Ca=c(380,620), 
-                      Wcapac=c(60,80,100,120,140,160,180,200))
-rundfr$PAW0 <- rundfr$Wcapac
-res1 <- RMATEsensitivity(rundfr)
+### run basic code
+out <- RMATE2()
 
-rundfr$NPP <- sapply(res1, function(x)mean(x$NPPtCha))
-rundfr$m <- sapply(res1, function(x)mean(x$m))
-rundfr$PAW <- sapply(res1, function(x)mean(x$PAWcur))
+
+
+#### run sensitivity test
+#rundfr <- expand.grid(#Ca=c(380,620), 
+#                      Wcapac=c(60,80,100,120,140,160,180,200))
+#rundfr$PAW0 <- rundfr$Wcapac
+#res1 <- RMATEsensitivity(rundfr)
+#
+#rundfr$NPP <- sapply(res1, function(x)mean(x$NPPtCha))
+#rundfr$m <- sapply(res1, function(x)mean(x$m))
+#rundfr$PAW <- sapply(res1, function(x)mean(x$PAWcur))
 
 # example
 #with(rundfr, plot(PAW0, PAW, pch=19, col=as.factor(Ca)))
